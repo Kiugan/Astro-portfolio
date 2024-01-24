88b7e0ef1e0a268403c8e298dbf8a52e69dd33e8
Kiugan <tec.medinaeze@gmail.com>
2024-01-24 16:04:22 -0300

    v1.5

--- src/pages/newFile.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../layouts/Layout.astro';
-
-<Fragment>
-<Layout title="Welcome to Astro.">
-<main>
-<h1>Hey, soy Kiugan <Badge>Disponible para trabajar</Badge></h1>
-</main>
-</Layout>
-</Fragment>;
